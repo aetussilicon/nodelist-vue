af4deae30cd974d1851014786bfae53d5cfd3121
Artur Oliveira <arturgaollidev@outlook.com>
2025-05-22 10:09:08 -0300

    refactor: altera classes do tailwind na tag main
    - Remove as classes e altera para atributos no próprio Main.css

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import Categories from '@/components/Categories.vue'
 </script>
 
 <template>
-  <main class="bg-[#121212] h-screen w-screen text-[#E0E0E0]">
-    <div class="mx-[20%]">
-      <h1 class="">Node List</h1>
+  <main>
+    <div class="mx-[5%]">
+      <h1>Node List</h1>
       <div class="flex items-center justify-center w-full">
         <Categories />
       </div>
