711a7dfa96081d2424defec0aad43d0c2bb18823
Artur Oliveira <arturgaollidev@outlook.com>
2025-05-23 18:46:33 -0300

    feat: Header base da página

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import Categories from '@/components/Categories.vue'
+import { Hexagon } from 'lucide-vue-next'
 </script>
 
 <template>
   <main>
-    <div class="mx-[5%]">
-      <h1>Node List</h1>
+    <div class="mx-[5%] py-5">
+      <div class="flex gap-2 mb-5 items-center">
+        <Hexagon size="50" />
+        <h1 class="text-xl font-bold">Node List</h1>
+      </div>
       <div class="flex items-center justify-center w-full">
         <Categories />
       </div>
     </div>
   </main>
-  </template>
+</template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
