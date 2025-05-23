29c81e9c3f2a5d8a169e19027d64a58399d30f4d
Artur Oliveira <arturgaollidev@outlook.com>
2025-05-23 19:34:15 -0300

    feat: adiciona ícone de lápis

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,11 +1,11 @@
 <script>
 import axios from 'axios'
-import { ChevronDown, Flag } from 'lucide-vue-next'
+import { ChevronDown, Flag, Pencil } from 'lucide-vue-next'
 import { PriorityLevels } from '../conts/PriorityLevels'
 
 export default {
   name: 'Categories-Card',
-  components: { ChevronDown, Flag },
+  components: { ChevronDown, Flag, Pencil },
   data() {
     return {
       categories: [],
@@ -23,13 +23,12 @@ export default {
         this.loading = true
         this.error = null
 
-        const res = await axios.get('http://localhost:8082/groups')
+        const res = await axios.get('http://localhost:8081/groups')
         this.categories = res.data
 
-        this.categories.forEach(cat => {
+        this.categories.forEach((cat) => {
           this.openCategories[cat.taskGroupId] = true
         })
-
       } catch (error) {
         this.error = 'Error while fetching categories: ' + error.message
         console.error(error)
@@ -41,26 +40,25 @@ export default {
       this.openCategories[id] = !this.openCategories[id]
     },
     getPrioriotyColor(priorityTitle) {
-      const priority = PriorityLevels.find((priority) => priority.label === priorityTitle);
-      return priority?.color;
-    }
-  }
+      const priority = PriorityLevels.find((priority) => priority.label === priorityTitle)
+      return priority?.color
+    },
+  },
 }
 </script>
 
 <template>
   <div class="flex flex-col w-full gap-2">
-    <div v-for="categorie in categories" :key="categorie.taskGroupId"
-    >
+    <div v-for="categorie in categories" :key="categorie.taskGroupId">
       <div class="bg-[#4CAF50] p-2 rounded-md flex flex-row justify-between items-center">
         <h1 class="text-md font-bold">{{ categorie.taskGroupName }}</h1>
         <button @click="toggleOpen(categorie.taskGroupId)" class="text-white">
           <ChevronDown
             class="text-white transition duration-300"
             :class="{
-              'ease-out' : openCategories[categorie.taskGroupId],
-              'rotate-180 ease-in' : !openCategories[categorie.taskGroupId]
-              }"
+              'ease-out': openCategories[categorie.taskGroupId],
+              'rotate-180 ease-in': !openCategories[categorie.taskGroupId],
+            }"
           />
         </button>
       </div>
@@ -69,17 +67,22 @@ export default {
         class="bg-[#1F1F1F] mt-3 p-2 rounded-md flex flex-col gap-3"
       >
         <div v-for="task in categorie.tasks" :key="task.taskId">
-         <div class="bg-[#121212] p-3 flex flex-col gap-3 rounded-md">
+          <div class="bg-[#121212] p-3 flex flex-col gap-3 rounded-md">
             <div class="flex items-center justify-between">
               <div class="flex gap-2 text-xl font-bold capitalize">
-               <input type="checkbox" />
-               <label>{{task.title}}</label>
-            </div>
+                <input type="checkbox" />
+                <label>{{ task.title }}</label>
+              </div>
 
-            <div>
-              <Flag :style="{ color: getPrioriotyColor(task.priority), fill:
-                getPrioriotyColor(task.priority) }" />
-            </div>
+              <div class="flex items-center gap-4">
+                <Flag
+                  :style="{
+                    color: getPrioriotyColor(task.priority),
+                    fill: getPrioriotyColor(task.priority),
+                  }"
+                />
+                <Pencil />
+              </div>
             </div>
             <div class="p-3 bg-[#1F1F1F] rounded-md">
               <textarea
@@ -91,8 +94,8 @@ export default {
             </div>
             <div class="h-1 w-full bg-[#1F1F1F] rounded-full my-2" />
             <div class="text-sm flex justify-between">
-              <p><strong>Criado em:</strong> {{task.createdAt}}</p>
-              <p><strong>Atualizado em:</strong> {{task.updatedAt}}</p>
+              <p><strong>Criado em:</strong> {{ task.createdAt }}</p>
+              <p><strong>Atualizado em:</strong> {{ task.updatedAt }}</p>
             </div>
           </div>
         </div>
